ebb1104039a76fe7769f449266f5fd7d19416262
Horace Yeung <horacehyeung@gmail.com>
2021-12-03 10:34:07 +1300

    day 1 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,9 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val numbers = input.map{ s -> s.toInt()}
+        val bigger = numbers.flatMapIndexed{ index, n -> if (index > 0 && n > numbers[index-1]) listOf(1) else listOf(0) }
+
+        return bigger.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +12,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+//    println(part2(input))
 }
